090e0a49819851b00d8759fa18e846d2bc9f81ab
Mahdi Rahmani <64958072+MahdiRahmani80@users.noreply.github.com>
2021-12-15 15:05:38 +0330

    Update Presenter.java

--- java/com/mahdirahmani8/mvp/Presenter/Presenter.java
+++ java/com/mahdirahmani8/mvp/Presenter/Presenter.java
@@ -1,5 +1,12 @@
 package com.mahdirahmani8.mvp.Presenter;
 
+/*
+    This calss we initialize MainContract.Presenter
+    and Create instace variable  MainContract.View
+    
+    and use Model's fountions like `show user`
+*/
+
 import android.view.View;
 
 import com.mahdirahmani8.mvp.MainContract;
